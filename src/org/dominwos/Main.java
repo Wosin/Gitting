25f3cb63093e48dc2e94ddab474c9cea424fa564
Wosiński Dominik <dominik.wosinski@developers.nbc.com.pl>
2018-06-23 10:34:07 +0200

    WelcomeMsg

--- src/org/dominwos/Main.java
+++ src/org/dominwos/Main.java
@@ -3,8 +3,9 @@ package org.dominwos;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("WITAJ UZYTKOWNIKU!");
         System.out.println("Działam Działam!");
         System.out.println("Master!");
+        System.out.println("Jeszcze TO!");
     }
 }
