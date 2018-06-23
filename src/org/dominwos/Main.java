a9d798f2c0673a721ffda2f1a9b270ec01ded064
Wosiński Dominik <dominik.wosinski@developers.nbc.com.pl>
2018-06-23 10:32:01 +0200

    Master

---
+++ src/org/dominwos/Main.java
@@ -0,0 +1,10 @@
+package org.dominwos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Działam Działam!");
+        System.out.println("Master!");
+    }
+}
